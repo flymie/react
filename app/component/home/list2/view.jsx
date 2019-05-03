74e60ccf4e396424f375dad0737e11fa215a5257
wuxiang <wuxiang@shein.com>
2019-05-03 14:09:56 +0800

    redux-saga

--- app/component/home/list2/view.jsx
+++ app/component/home/list2/view.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { increaseAction } from './action';
@@ -19,7 +18,6 @@ class Container extends React.Component {
   }
 
   render() {
-    const arr = [1, 2, 3];
     const { value, dispatch } = this.props;
     return (
       <div className="a">
@@ -35,21 +33,7 @@ class Container extends React.Component {
             getMusic
           </button>
         </div>
-        <h1 className="a_b">hello, world!</h1>
-        <p className="background">test speed</p>
-        <img src={require('Images/userImg240.png')} alt="" />
-        {
-          arr.map((v, index) => (
-            <p key={`${index + 1}`}>{v}</p>
-          ))
-        }
-        <Link to={`/routerV4/1?a=${encodeURIComponent('4&&-.2')}&b=${encodeURIComponent('5=&')}`}>传入param,query</Link>
-        <p>
-          <Link to={'/uRLSearchParams/1?a=4&&-.2)}&b=5=&'}>通过URLSearchParams解析查询的字段</Link>
-        </p>
-        <p>
-          <Link to="/home/list2">homeList2</Link>
-        </p>
+        <h1 className="a_b">homeList2</h1>
       </div>
     );
   }
--- app/routes/home.js
+++ app/routes/home.js
@@ -1,10 +1,16 @@
 import Container from '../component/home/list/view';
+import homeList2 from '../component/home/list2/view';
 
 const home = [
   {
     exact: true,
     path: '/',
     component: Container,
+  },
+  {
+    exact: true,
+    path: '/home/list2',
+    component: homeList2,
   },
 ];
 
