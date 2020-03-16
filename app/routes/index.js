1d8c0ba0235a9fc6b2bdc0686a88678d49844ffa
wuxiang <wuxiang@shein.com>
2020-03-16 13:30:17 +0800

    ts

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,12 +2,14 @@ import home from './home';
 import routerV4 from './routerV4';
 import error from './error';
 import learn from './learn';
+import ts from './ts';
 
 const routes = [
   ...home,
   ...routerV4,
   ...error,
   ...learn,
+  ...ts,
 ];
 
 export default routes;
---
+++ app/routes/ts.js
@@ -0,0 +1,12 @@
+import Button from '../component/TS/view';
+
+const ts = [
+  {
+    exact: true,
+    path: '/button',
+    component: Button,
+  },
+
+];
+
+export default ts;
