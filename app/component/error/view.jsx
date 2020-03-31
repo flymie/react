66e5f61b6be444212b1668b5eb81de34393d67f1
wuxiang <wuxiang@shein.com>
2020-03-31 16:55:27 +0800

    动态载入

--- app/component/error/view.jsx
+++ app/component/error/view.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-
+import React, { Suspense } from 'react';
 class Page404 extends React.Component {
   componentDidMount() {
   }
@@ -10,10 +9,32 @@ class Page404 extends React.Component {
   componentWillUnmount() {
   }
 
+  renderDetail(pageType) {
+    const dynamicDetail = React.lazy(() => import(`./test/${pageType}.jsx`));
+    return dynamicDetail;
+  }
+
   render() {
+    const pageType = ['view', 'view2', 'view3'];
+
+    const result = pageType.map(v => this.renderDetail(v));
+    function Story(v) {
+      const SpecificStory = v;
+      return <SpecificStory />;
+    }
     return (
       <div>
         <h1>404，可能搞错了～</h1>
+        {/*{result}*/}
+        <Suspense fallback={<div>Loading...</div>}>
+          {
+            result.map((v, ind) => (
+              <React.Fragment key={ind}>
+                {Story(v)}
+              </React.Fragment>
+            ))
+          }
+        </Suspense>
       </div>
     );
   }
